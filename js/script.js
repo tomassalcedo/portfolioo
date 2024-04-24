9fa7baf028d3266f1b352bba0c9052802b4fb55d
Tomas Salcedo <tomas.river12[200~>
2024-04-23 22:02:57 -0300

    cambios

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal();
+
+
+// sr.reveal('.container', {
+//     duration: 3000,
+//     origin: 'left',
+//     distance: '-400px' 
+// });
+
+
+// sr.reveal('.container-sections', {
+//     duration: 4000,
+//     origin: 'bottom',
+//     distance: '-100px' 
+// });
+
+var containerOptions = {
+    duration: 2000,
+    origin: 'left',
+    distance: '-400px',
+    reset: true // Es 
+};
+
+
+var containerSectionsOptions = {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px',
+    reset: true
+};
+
+
+sr.reveal('.container', containerOptions);
+sr.reveal('.container-sections', containerSectionsOptions);
+
